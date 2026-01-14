be91642a402a55541761c2b919c9c447f7fc2add
Viet <cloudcuaviet@gmail.com>
2026-01-14 09:38:43 +0700

    add layout

--- ECommereWeb/Program.cs
+++ ECommereWeb/Program.cs
@@ -1,7 +1,12 @@
+using ECommereWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<Hshop2023Context>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("Hshop")));
 
 var app = builder.Build();
 
