6b2234438ad613735bf43d97a9382dbcbf638e24
Viet <cloudcuaviet@gmail.com>
2026-01-14 12:01:15 +0700

    add picture in eb

--- ECommereWeb/Controllers/HangHoaController.cs
+++ ECommereWeb/Controllers/HangHoaController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommereWeb.Data;
+using ECommereWeb.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommereWeb.Controllers
 {
     public class HangHoaController : Controller
     {
+        private readonly Hshop2023Context db;
+        public HangHoaController(Hshop2023Context context)
+        {
+            db = context;
+        }
         public IActionResult Index(int? loai)
         {
-            return View();
+            var hangHoas = db.HangHoas.AsQueryable();
+            if(loai.HasValue)
+            {
+                hangHoas = hangHoas.Where(p => p.MaLoai == loai.Value);
+            }
+            var result = hangHoas.Select(p => new HangHoaVM
+            {
+                MaHh = p.MaHh,
+                TenHh = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            return View(result);
         }
     }
 }
---
+++ ECommereWeb/ViewModels/HangHoaVM.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommereWeb.ViewModels
+{
+    public class HangHoaVM
+    {
+        public int MaHh { get; set; }
+        public string TenHh { get; set; } = string.Empty;
+        public double DonGia { get; set; }
+        public string Hinh { get; set; } = string.Empty;
+        public string MoTaNgan { get; set; } = string.Empty;
+        public string TenLoai { get; set; }
+    }
+}
