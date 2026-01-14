0f5d18f1a557e078228c1dd3cafdd6e3323ab37e
Viet <cloudcuaviet@gmail.com>
2026-01-14 16:37:49 +0700

    Command code

--- ECommereWeb/Controllers/HangHoasController.cs
+++ ECommereWeb/Controllers/HangHoasController.cs
@@ -12,7 +12,6 @@ namespace ECommereWeb.Controllers
     public class HangHoasController : Controller
     {
         private readonly Hshop2023Context _context;
-
         public HangHoasController(Hshop2023Context context)
         {
             _context = context;
@@ -89,7 +88,7 @@ namespace ECommereWeb.Controllers
             return View(hangHoa);
         }
 
-        // POST: HangHoas/Edit/5
+        // POST: HangHoas/Edit/555
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
