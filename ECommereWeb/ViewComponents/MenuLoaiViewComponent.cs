9a42094cda7a64dab3e9f852e3391170fd8b3449
Viet <cloudcuaviet@gmail.com>
2026-01-14 10:47:18 +0700

    add Defautl for ViewComponents

--- ECommereWeb/ViewComponents/MenuLoaiViewComponent.cs
+++ ECommereWeb/ViewComponents/MenuLoaiViewComponent.cs
@@ -1,6 +1,23 @@
-﻿namespace ECommereWeb.ViewComponents
+﻿using ECommereWeb.Data;
+using ECommereWeb.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommereWeb.ViewComponents
 {
-    public class MenuLoaiViewComponent
+    public class MenuLoaiViewComponent : ViewComponent
     {
+        private readonly Hshop2023Context db;
+        public MenuLoaiViewComponent(Hshop2023Context context) => db = context;
+
+        public IViewComponentResult Invoke()
+        {
+            var data = db.Loais.Select(lo => new MenuLoaiVM
+            {
+                MaLoai = lo.MaLoai,
+                TenLoai = lo.TenLoai,
+                SoLuong = lo.HangHoas.Count
+            });
+            return View(data); //default view name is "Default"
+        }
     }
 }
---
+++ ECommereWeb/ViewModels/MenuLoaiVM.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommereWeb.ViewModels
+{
+    public class MenuLoaiVM
+    {
+        public int MaLoai { get; set; }
+        public string TenLoai { get; set; } = null!;
+        public int SoLuong { get; set; }
+    }
+}
